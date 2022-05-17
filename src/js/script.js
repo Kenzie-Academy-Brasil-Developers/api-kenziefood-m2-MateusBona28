4273115076bdc3920ba0cbceabb8f4f5985fcfdb
Willian <willianx64@gmail.com>
2022-05-17 10:43:38 -0300

    fix: modal second step

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,20 @@
 import Api from "./Api.js";
 
-const cartHeadder = document.getElementById('cart-header')
+const cartHeadder = document.getElementById('cart-header');
+const modalCart = document.getElementById('modal-cart');
+const closeModal = document.getElementsByClassName('close-modal');
 
+cartHeadder.addEventListener('click', displayModal)
 
-cartHeadder.addEventListener('click', () => {
-    
-})
+function displayModal() {
+    modalCart.style.display = 'flex';
+}
+
+function closeModalFunctionality() {
+    for(let i = 0; i < closeModal.length; i++) {
+        closeModal[i].addEventListener('click', (event) => {
+            modalCart.style.display = 'none'
+        })
+    }
+}
+closeModalFunctionality()
