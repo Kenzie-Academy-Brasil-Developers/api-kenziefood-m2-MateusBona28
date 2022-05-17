74a8d248379e824ba685778a77b29c5bb64ee323
Mateus Bonacina <89853026+MateusBona28@users.noreply.github.com>
2022-05-17 12:17:23 -0300

    fix: arrumei a função que fecha os modais para fechar também os modais de cadastro e login

--- src/js/script.js
+++ src/js/script.js
@@ -2,8 +2,10 @@ import Api from "./Api.js";
 
 const cartHeadder = document.getElementById('cart-header');
 const modalCart = document.getElementById('modal-cart');
-const btnRegisterForm = document.getElementById("register-button")
-const btnLoginForm = document.getElementById("login-button")
+const modalLogin = document.getElementById('login')
+const modalRegister = document.getElementById('register')
+const btnRegisterForm = document.getElementById('register-button')
+const btnLoginForm = document.getElementById('login-button')
 const closeModal = document.getElementsByClassName('close-modal');
 
 cartHeadder.addEventListener('click', displayModal)
@@ -18,6 +20,8 @@ function closeModalFunctionality() {
     for(let i = 0; i < closeModal.length; i++) {
         closeModal[i].addEventListener('click', (event) => {
             modalCart.style.display = 'none'
+            modalLogin.style.display = 'none'
+            modalRegister.style.display = 'none'
         })
     }
 }
