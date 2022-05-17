ff44586dbd81d9530f2db2a98ed3f001eb47aae4
Mateus Bonacina <89853026+MateusBona28@users.noreply.github.com>
2022-05-17 11:44:27 -0300

    feat: novas features de registro e login , todas funcionais e testadas! Ainda sem aviso  de erro ou sucesso.

--- src/js/Api.js
+++ src/js/Api.js
@@ -1,6 +1,42 @@
 //criar class para acessar o api
 class Api {
 
+    static baseUrl = "https://api-kenzie-food.herokuapp.com"
+
+    static async getUserRegister(data) {
+
+        const registerUrl = "/auth/register"
+
+        const response = await fetch(`${this.baseUrl}${registerUrl}`,{
+            method: "POST",
+            headers: {
+                "Content-Type" : "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+
+        const newData = response.json()
+
+        localStorage.setItem("token", newData.token)
+        localStorage.setItem("userId", newData.userId)
+    }
+
+    static async getUserLogin(data) {
+
+        const loginUrl = "/auth/login"
+
+        const response = await fetch(`${this.baseUrl}${loginUrl}`,{
+            method: "POST",
+            headers: {
+                "Content-Type" : "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+
+        const newData = response.json()
+
+        console.log(newData)
+    }
 }
 
 export default Api;
--- src/js/script.js
+++ src/js/script.js
@@ -2,9 +2,13 @@ import Api from "./Api.js";
 
 const cartHeadder = document.getElementById('cart-header');
 const modalCart = document.getElementById('modal-cart');
+const btnRegisterForm = document.getElementById("register-button")
+const btnLoginForm = document.getElementById("login-button")
 const closeModal = document.getElementsByClassName('close-modal');
 
 cartHeadder.addEventListener('click', displayModal)
+btnRegisterForm.addEventListener("click", registerNewUser)
+btnLoginForm.addEventListener("click", logUser)
 
 function displayModal() {
     modalCart.style.display = 'flex';
@@ -18,3 +22,47 @@ function closeModalFunctionality() {
     }
 }
 closeModalFunctionality()
+
+async function registerNewUser(event) {
+    
+    event.preventDefault()
+
+    const registerForm = document.getElementById("form-register")
+
+    const userInfo = registerForm.elements
+
+    const newUser = {}
+
+    for(let i = 0; i < userInfo.length; i++){
+
+        const info = userInfo[i]
+
+        if(userInfo[i].value !== ""){
+            newUser[info.name] = info.value
+        }
+    }
+
+    await Api.getUserRegister(newUser)
+}
+
+async function logUser(event) {
+
+    event.preventDefault()
+
+    const formLogin = document.getElementById("form-login")
+
+    const loginInfo = formLogin.elements
+
+    const user = {}
+
+    for(let i = 0; i < loginInfo.length; i++){
+
+        const info = loginInfo[i]
+
+        if(info.value !== ""){
+            user[info.name] = info.value
+        }
+    }
+
+    await Api.getUserLogin(user)
+}
