fa941d0161302e6685fa2a4eea994d73942619d2
Tales <jobbtales@gmail.com>
2022-05-17 12:16:23 -0300

    fix: function productsHomePage realocada para dashboardScript

--- src/js/dashboardScript.js
+++ src/js/dashboardScript.js
@@ -1,2 +1,48 @@
 import Api from "./Api.js";
 
+function productsHomePage(products) {
+    const containerCards = document.querySelector('.container-cards')
+
+    products.forEach(element => {
+        const card = document.createElement('article')
+        card.classList.add('card')
+        card.dataset.id = element.id
+
+        const figure = document.createElement('figure')
+        const img = document.createElement('img')
+        img.src = element.imagem
+        img.alt = element.nome
+
+        figure.appendChild(img)
+
+        const cardBody = document.createElement('div')
+        cardBody.classList.add('card-body')
+
+        const cardBodyTitle = document.createElement('h2')
+        cardBodyTitle.innerText = element.nome
+        const CardBodyDesc = document.createElement('p')
+        CardBodyDesc.classList.add('card-desc')
+        CardBodyDesc.innerText = element.descricao
+
+        cardBody.append(cardBodyTitle, CardBodyDesc)
+
+        const cardCategory = document.createElement('div')
+        cardCategory.classList.add('card-category')
+        cardCategory.innerText = element.categoria
+
+        const cardFooter = document.createElement('div')
+        const cardFooterPrice = document.createElement('p')
+        const cardFooterAddCart = document.createElement('button')
+
+        cardFooterPrice.innerText = element.preco
+        cardFooterAddCart.innerText = 'Add'
+        cardFooterAddCart.dataset.id = element.id
+        cardFooter.append(cardFooterPrice, cardFooterAddCart)
+
+        card.append(figure, cardBody, cardCategory, cardFooter)
+
+        containerCards.append(card)
+    })
+}
+
+export default productsHomePage
--- src/js/script.js
+++ src/js/script.js
@@ -1,46 +1,7 @@
 import Api from "./Api.js";
+import productsHomePage from "./dashboardScript.js"
 
-async function productsHomePage(products) {
-    const containerCards = document.querySelector('.container-cards')
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEzZWViNTk3LTUzZGQtNDQ0MS04M2ZmLTkzMjIzYjFiY2Q5MyIsImlhdCI6MTY1Mjc5NTc3OCwiZXhwIjoxNjUzNjU5Nzc4LCJzdWIiOiJbb2JqZWN0IFVuZGVmaW5lZF0ifQ.8g38AzX3nY_zbsi_fCf9JCvakfxgcSUHsm5GrTSGNdM'
+const produtos = await Api.getPrivateProducts(token)
 
-    await products.forEach(element => {
-        const card = document.createElement('article')
-        card.classList.add('card')
-        card.dataset.id = element.id
-
-        const figure = document.createElement('figure')
-        const img = document.createElement('img')
-        img.src = element.imagem
-        img.alt = element.nome
-
-        figure.appendChild(img)
-
-        const cardBody = document.createElement('div')
-        cardBody.classList.add('card-body')
-
-        const cardBodyTitle = document.createElement('h2')
-        cardBodyTitle.innerText = element.nome
-        const CardBodyDesc = document.createElement('p')
-        CardBodyDesc.classList.add('card-desc')
-        CardBodyDesc.innerText = element.descricao
-
-        cardBody.append(cardBodyTitle, CardBodyDesc)
-
-        const cardCategory = document.createElement('div')
-        cardCategory.classList.add('card-category')
-        cardCategory.innerText = element.categoria
-
-        const cardFooter = document.createElement('div')
-        const cardFooterPrice = document.createElement('p')
-        const cardFooterAddCart = document.createElement('button')
-
-        cardFooterPrice.innerText = element.preco
-        cardFooterAddCart.innerText = 'Add'
-        cardFooterAddCart.dataset.id = element.id
-        cardFooter.append(cardFooterPrice, cardFooterAddCart)
-
-        card.append(figure, cardBody, cardCategory, cardFooter)
-
-        containerCards.append(card)
-    })
-}
+productsHomePage(produtos)
