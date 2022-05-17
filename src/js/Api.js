6c4c064e385aae90cbfe3e59fb5d165e292604d7
Willian <willianx64@gmail.com>
2022-05-17 11:01:31 -0300

    feat: Api.register

--- src/js/Api.js
+++ src/js/Api.js
@@ -1,6 +1,23 @@
 //criar class para acessar o api
 class Api {
 
+    static baseURL = 'https://api-kenzie-food.herokuapp.com/'
+    
+    static register(data) {
+
+        return fetch(`${Api.baseURL}/register`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .catch(err => console.log(err))
+    }
+
 }
+
+        
 
 export default Api;
