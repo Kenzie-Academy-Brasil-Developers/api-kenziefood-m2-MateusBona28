c9b8e2bcdb86e08a83da8e4734c8d2e57b18c23e
Tales <jobbtales@gmail.com>
2022-05-17 11:49:41 -0300

    feat: consumindo produtos API homepage

--- src/js/Api.js
+++ src/js/Api.js
@@ -1,6 +1,33 @@
 //criar class para acessar o api
 class Api {
+    static async getPublicProducts() {
+        const response = await fetch('https://api-kenzie-food.herokuapp.com/products', {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            },
+        })
+        .then(response => response.json())
+        .then(response => response)
+        .catch(error => error)
 
+        return response
+    }
+
+    static async getPrivateProducts(token) {
+        const response = await fetch('https://api-kenzie-food.herokuapp.com/my/products', {
+            method: "GET",
+            headers: {
+                "Authorization": `Bearer ${token}`,
+                "Content-Type": "application/json"
+            },
+        })
+        .then(response => response.json())
+        .then(response => response)
+        .catch(error => error)
+
+        return response
+    }
 }
 
 export default Api;
